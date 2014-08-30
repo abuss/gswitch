5a33f0ea3ed4810792ee0fb5797098d937da9489
Antal A. Buss <abuss@cs.tamu.edu>
2014-08-30 16:39:53 -0500

    Fixed guard evaluation with char*

--- gswitch.hpp
+++ gswitch.hpp
@@ -14,10 +14,11 @@ namespace gswitch {
 namespace guard {
     
 template <typename Guard, typename T>
-bool eval_guard(const Guard& g, const T& x)
+bool eval_guard(Guard g, T x)
 {
   return false;
 }
+
 
 template <template<typename,typename> class C, typename T, typename A>
 bool eval_guard(const std::initializer_list<T>& guard, const C<T,A>& x)
@@ -25,18 +26,13 @@ bool eval_guard(const std::initializer_list<T>& guard, const C<T,A>& x)
   return (x.size()==guard.size()) && (std::equal(x.begin(),x.end(),guard.begin()));
 }
 
-template <typename T>
-bool eval_guard(const std::initializer_list<T>& guard, const T& x)
+
+template <typename T, typename U>
+bool eval_guard(const std::initializer_list<T>& guard, const U& x)
 {
   return (std::find(guard.begin(),guard.end(),x)!=guard.end());    
 }
 
-template <>
-bool eval_guard(const std::initializer_list<const char*>& guard, 
-                const std::string& x)
-{
-  return (std::find(guard.begin(),guard.end(),x)!=guard.end());    
-}
 
 template <typename T>
 bool eval_guard(const T& g, const T& x)
@@ -48,6 +44,12 @@ bool eval_guard(const char* g, const std::string& x)
 {
   return x.compare(g)==0;    
 }
+
+bool eval_guard(const char* g, const char* x)
+{
+  return eval_guard(g, std::string(x));
+}
+
 
 template <typename Guard>
 struct case_fn
